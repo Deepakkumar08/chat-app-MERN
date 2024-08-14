3006e370e7dd6f846a568ae5717454ed91782d68
Deepakkumar08 <vdeepakvaithe@gmail.com>
2024-08-14 21:40:05 +0530

    14-08 updates

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,12 +6,13 @@ import {NavLink} from  'react-router-dom';
 import Avatar from "./Avatar"
 import {useSelector} from 'react-redux'
 import EditUserInfo from './EditUserInfo';
+import Divider from './Divider';
 
 const Sidebar = () => {
   const user = useSelector(state => state?.user)
   const [editUserOpen, setEditUserOpen] = useState(false)
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
         <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 flex flex-col justify-between'>
           <div>
             <NavLink className={({isActive})=>`w-12 flex h-12 justify-center items-center cursor-pointer  hover:bg-slate-200 ${isActive && 'bg-slate-200' }`} title='chat'>
@@ -32,6 +33,15 @@ const Sidebar = () => {
 
           </button>
         </div>
+        </div>
+        <div className='w-full'>
+          <div className='h-16 flex items-center'>
+          <h2  className='text-lg font-bold p-4  text-slate-600'>Message</h2> 
+          
+          </div>
+          <div className='bg-slate-200 p-[0.5px]'> </div>
+
+            <div className='bg-red-500  h-[cal(100vh-64px)] overflow-x-hidden overflow-y-scroll'></div>
         </div>
     {
       editUserOpen && <EditUserInfo onClose={()=>setEditUserOpen(false)} user={user}></EditUserInfo>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useDispatch,useSelector } from "react-redux";
 import { logout, setUser } from "../redux/userSlice";
 import Sidebar from "../components/Sidebar";
+import chatapplogo from '../assests/chatapplogo.jpg';
     
 const Home = ()=>{
     const user = useSelector(state=> state.user)
@@ -16,7 +17,7 @@ const Home = ()=>{
            const res = await axios('/api/user-Details',{withCredentials:true}) 
            console.log(res,'current user');
            dispatch(setUser(res?.data?.data[0]))
-           if(res.data.logout){
+           if(res.data.data.logout){
                 dispatch(logout())
                 navigate('/email')
            }
@@ -27,14 +28,23 @@ const Home = ()=>{
     useEffect(()=>{
         fetchUserData()
     },[])
+    const basePath =location.pathname==='/'
     return (
         <div className="grid lg:grid-cols-[300px,1fr] h-screen max-h-screen">
-            <section className="bg-white rounded-tr" >
+            <section className={`bg-white rounded-tr ${!basePath && 'hidden'} lg:block`} >
                 <Sidebar></Sidebar>
             </section>
-            <section>
-                <Outlet></Outlet>
+            <section className={`${basePath && 'hidden'}`}>
+                <Outlet></Outlet> 
             </section>
+            <div className="lg:flex justify-center items-center flex-col gap-2 hidden">
+                <div>
+                    <img
+                        src={chatapplogo} width={200} alt="logo">
+                    </img>
+                </div>
+                <p className="text-lg mt-2 text-slate-500">Please select user to send message</p>
+            </div>
         </div>
     )
 }
